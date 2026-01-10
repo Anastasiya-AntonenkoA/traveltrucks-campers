399f77726448b48ee6baad87305e065ffc6e68a8
anastasiia <Nastyaantonenkoa@gmail.com>
2026-01-10 22:24:39 +0200

    pagination (numbers)

--- components/CamperList/CamperList.tsx
+++ components/CamperList/CamperList.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import { Camper } from "@/lib/api";
 import CamperCard from "../CamperCard/CamperCard";
 import css from "./CamperList.module.css";
+import { useState } from "react";
+import Pagination from "../Pagination/Pagination";
 
 
 type Props = {
@@ -8,20 +12,110 @@ type Props = {
 };
 
 const CamperList = ({ campers }: Props) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const itemsPerPage = 4;
+
+    const totalPages = Math.ceil(campers.length / itemsPerPage);
+
+    const indexOfLastCamper = currentPage * itemsPerPage;
+    const indexOfFirstCamper = indexOfLastCamper - itemsPerPage;
+    const currentCampers = campers.slice(indexOfFirstCamper, indexOfLastCamper);
+
+    const handlePageChange = (page: number) => {
+    setCurrentPage(page);
+    // Опціонально: скрол вгору при зміні сторінки
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+    
+    
     return (
         <div className={css.container}>
             <aside className={css.filters}>
-                {/* Тут будуть ваші фільтри */}
-                <p>Filters section</p>
+                {/* Location Section */}
+            <div className={css.filterGroup}>
+                <h3 className={css.labelTitle}>Location</h3>
+                    <div className={css.inputWrapper}>
+                        <svg width="18" height="20" className={css.mapIcon}>
+                        <use href="/icons/sprite.svg#icon-map" />
+                        </svg>
+                        <input 
+                        type="text"
+                        placeholder="Kyiv, Ukraine" 
+                        className={css.locationInput} 
+                        />
+                    </div>
+            </div>
+
+            <p className={css.filtersText}>Filters</p>
+
+            {/* Vehicle Equipment Section */}
+            <div className={css.filterGroup}>
+                <h3 className={css.sectionTitle}>Vehicle equipment</h3>
+                <hr className={css.divider} />
+                <div className={css.checkboxGrid}>
+                {/* Кожен елемент можна винести в окремий компонент FilterCard */}
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>🌬️</span>
+                    <span>AC</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>⚙️</span>
+                    <span>Automatic</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>☕</span>
+                    <span>Kitchen</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>📺</span>
+                    <span>TV</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>🚿</span>
+                    <span>Bathroom</span>
+                </button>
+                </div>
+            </div>
+
+            {/* Vehicle Type Section */}
+            <div className={css.filterGroup}>
+                <h3 className={css.sectionTitle}>Vehicle type</h3>
+                <hr className={css.divider} />
+                <div className={css.checkboxGrid}>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>🚐</span>
+                    <span>Van</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>🚍</span>
+                    <span>Fully Integrated</span>
+                </button>
+                <button type="button" className={css.filterCard}>
+                    <span className={css.icon}>🚐</span>
+                    <span>Alcove</span>
+                </button>
+                </div>
+            </div>
+
+            {/* Search Button */}
+            <button type="submit" className={css.searchButton}>
+                Search
+            </button>
             </aside>
 
             <main className={css.catalog}>
-                {/* Ваш список <CamperList /> опиниться тут */}
                 <ul className={css.list}>
-                    {campers.map((camper) => (
-                    <CamperCard key={camper.id} item={camper} />
+                    {currentCampers.map((camper) => (
+                        <CamperCard key={camper.id} item={camper} />
                     ))}
                 </ul>
+                <div className={css.paginationWrapper}>
+                    <Pagination 
+                        totalPages={totalPages} 
+                        page={currentPage} 
+                        onChange={handlePageChange} 
+                    />
+                </div>  
             </main>
         </div>
     );
