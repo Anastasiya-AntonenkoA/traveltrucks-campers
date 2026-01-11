642ad9f887f95d595b46ecd23eb54c1e3b9268e4
anastasiia <Nastyaantonenkoa@gmail.com>
2026-01-11 13:24:19 +0200

    filters

--- components/CamperList/CamperList.tsx
+++ components/CamperList/CamperList.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { Camper, getCampers } from "@/lib/api";
+import { Camper, getCampers, FilterParams } from "@/lib/api";
 import CamperCard from "../CamperCard/CamperCard";
 import css from "./CamperList.module.css";
+import Filters from "../Filters/Filters";
 import { useState } from "react";
 
 
 type Props = {
-    initialData: {
-    items: Camper[];
-    total: number;
-    };
+    initialData: { items: Camper[]; total: number };
 };
 
 const CamperList = ({ initialData }: Props) => {
@@ -18,114 +16,42 @@ const CamperList = ({ initialData }: Props) => {
     const [page, setPage] = useState(1);
     const [isLoading, setIsLoading] = useState(false);
     const [isEnd, setIsEnd] = useState(initialData.items.length < 4);
+    const [currentFilters, setCurrentFilters] = useState<FilterParams | undefined>(undefined);
 
-    const handleLoadMore = async () => {
-        const nextPage = page + 1;
+    const fetchCampers = async (nextPage: number, filters: FilterParams | undefined, isNewSearch: boolean) => {
         setIsLoading(true);
-
         try {
-            const data = await getCampers(nextPage, 4);
-            if (data.items.length < 4) {
-                setIsEnd(true);
+            const data = await getCampers(nextPage, 4, filters ?? undefined); 
+            
+            if (isNewSearch) {
+                setCampers(data.items);
+                setPage(1);
+            } else {
+                setCampers((prev) => [...prev, ...data.items]);
+                setPage(nextPage);
             }
-            setCampers((prev) => [...prev, ...data.items]);
-            setPage(nextPage);
+            
+            setIsEnd(data.items.length < 4);
         } catch (error) {
             console.error(error);
         } finally {
             setIsLoading(false);
         }
     };
-    
+
+    const handleSearch = (filters: FilterParams) => {
+        setCurrentFilters(filters);
+        fetchCampers(1, filters, true);
+    };
+
+    const handleLoadMore = () => {
+        fetchCampers(page + 1, currentFilters, false);
+    };
+
     return (
         <div className={css.container}>
-            <aside className={css.filters}>
-                {/* Location Section */}
-            <div className={css.filterGroup}>
-                <h3 className={css.labelTitle}>Location</h3>
-                <div className={css.inputWrapper}>
-                    <svg width="18" height="20" className={css.mapIcon}>
-                    <use href="/icons/sprite.svg#icon-map" />
-                    </svg>
-                    <input 
-                    type="text"
-                    placeholder="Kyiv, Ukraine" 
-                    className={css.locationInput} 
-                    />
-                </div>
-            </div>
-
-            <p className={css.filtersText}>Filters</p>
-
-            {/* Vehicle Equipment Section */}
-            <div className={css.filterGroup}>
-                <h3 className={css.sectionTitle}>Vehicle equipment</h3>
-                <hr className={css.divider} />
-                <div className={css.checkboxGrid}>
-                    <button type="button" className={css.filterCard}>
-                        <svg width="32" height="32" className={css.icon}>
-                            <use href="/icons/sprite.svg#icon-ac" />
-                        </svg>
-                        <span>AC</span>
-                    </button>
-                    <button type="button" className={css.filterCard}>
-                        <svg width="32" height="32" className={css.icon}>
-                            <use href="/icons/sprite.svg#icon-automatic" />
-                        </svg>
-                        <span>Automatic</span>
-                    </button>
-                    <button type="button" className={css.filterCard}>
-                        <svg width="32" height="32" className={css.icon}>
-                            <use href="/icons/sprite.svg#icon-kitchen" />
-                        </svg>
-                        <span>Kitchen</span>
-                    </button>
-                    <button type="button" className={css.filterCard}>
-                        <svg width="32" height="32" className={css.icon}>
-                            <use href="/icons/sprite.svg#icon-tv" />
-                        </svg>
-                        <span>TV</span>
-                    </button>
-                    <button type="button" className={css.filterCard}>
-                        <svg width="32" height="32" className={css.icon}>
-                            <use href="/icons/sprite.svg#icon-bathroom" />
-                        </svg>
-                        <span>Bathroom</span>
-                    </button>
-                </div>
-            </div>
-
-            {/* Vehicle Type Section */}
-            <div className={css.filterGroup}>
-                <h3 className={css.sectionTitle}>Vehicle type</h3>
-                <hr className={css.divider} />
-                <div className={css.checkboxGrid}>
-                <button type="button" className={css.filterCard}>
-                    <svg width="32" height="32" className={css.icon}>
-                        <use href="/icons/sprite.svg#icon-van" />
-                    </svg>
-                    <span>Van</span>
-                </button>
-                <button type="button" className={css.filterCard}>
-                    <svg width="32" height="32" className={css.icon}>
-                        <use href="/icons/sprite.svg#icon-fully-integrated" />
-                    </svg>
-                    <span>Fully Integrated</span>
-                </button>
-                <button type="button" className={css.filterCard}>
-                    <svg width="32" height="32" className={css.icon}>
-                        <use href="/icons/sprite.svg#icon-alcove" />
-                    </svg>
-                    <span>Alcove</span>
-                </button>
-                </div>
-            </div>
-
-            {/* Search Button */}
-            <button type="submit" className={css.searchButton}>
-                Search
-            </button>
-            </aside>
+            <Filters onSearch={handleSearch} />
+            
             <main className={css.catalog}>
                 <ul className={css.list}>
                     {campers.map((camper) => (
--- lib/api.ts
+++ lib/api.ts
@@ -37,6 +37,16 @@ export type Camper = {
     }[];
 };
 
+export type FilterParams = {
+    location?: string;
+    form?: string;
+    AC?: boolean;
+    transmission?: string;
+    kitchen?: boolean;
+    TV?: boolean;
+    bathroom?: boolean;
+};
+
 // відповіді від бекенду
 export type CampersResponse = {
     total: number;
@@ -46,13 +56,29 @@ export type CampersResponse = {
 axios.defaults.baseURL = "https://66b1f8e71ca8ad33d4f5f63e.mockapi.io"; //мій
 // axios.defaults.baseURL = "https://69625c65d9d64c761907adb2.mockapi.io";
 
-export const getCampers = async (page: number = 1, limit: number = 4) => {
-    const res = await axios.get<CampersResponse>("/campers", {
-        params: {
-            page: page,
-            limit: limit
-        }
-    });
+export const getCampers = async (
+    page: number = 1, 
+    limit: number = 4, 
+    filters: FilterParams = {} 
+) => {
+    const params: Record<string, string | number | boolean> = {
+        page,
+        limit,
+    };
+
+    if (filters.location) params.location = filters.location;
+    if (filters.form) params.form = filters.form;
+    
+    if (filters.AC) params.AC = true;
+    if (filters.kitchen) params.kitchen = true;
+    if (filters.TV) params.TV = true;
+    if (filters.bathroom) params.bathroom = true;
+    
+    if (filters.transmission === "automatic") {
+        params.transmission = "automatic";
+    }
+
+    const res = await axios.get<CampersResponse>("/campers", { params });
     return res.data;
 };
 
