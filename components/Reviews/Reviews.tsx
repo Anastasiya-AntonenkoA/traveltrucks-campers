36d758f13ee6b5f86f022701bbccffe3a8220032
anastasiia <Nastyaantonenkoa@gmail.com>
2026-01-11 16:46:19 +0200

    reviews block

--- components/Reviews/Reviews.tsx
+++ components/Reviews/Reviews.tsx
@@ -1,22 +1,46 @@
 import { Camper } from "@/lib/api";
+import css from "./Reviews.module.css";
 
 interface ReviewsProps {
   reviews: Camper["reviews"];
 }
 
 const Reviews = ({ reviews }: ReviewsProps) => {
-  if (!reviews || reviews.length === 0) return <p>No reviews yet.</p>;
+    if (!reviews || reviews.length === 0) return <p>No reviews yet.</p>;
+    
+  const renderStars = (rating: number) => {
+    return Array.from({ length: 5 }, (_, i) => (
+      <svg
+        key={i}
+        width="16"
+        height="16"
+        className={`${css.starIcon} ${i < rating ? css.starFilled : ""}`}
+      >
+        <use href="/icons/sprite.svg#icon-star" />
+      </svg>
+    ));
+  };
 
   return (
-    <div>
+    <div className={css.container}>
       {reviews.map((review, index) => (
-        <div key={index} style={{ marginBottom: "24px" }}>
-          {/* Тут буде логіка відображення імені, зірочок та коментаря */}
-          <h4>{review.reviewer_name}</h4>
-          <p>{review.comment}</p>
+        <div key={index} className={css.reviewItem}>
+          <div className={css.reviewerHeader}>
+            <div className={css.avatar}>
+              {review.reviewer_name.charAt(0).toUpperCase()}
+            </div>
+            <div className={css.infoWrapper}>
+              <h4 className={css.name}>{review.reviewer_name}</h4>
+              <div className={css.starsWrapper}>
+                {renderStars(review.reviewer_rating)}
+              </div>
+            </div>
+          </div>
+
+          <p className={css.comment}>{review.comment}</p>
         </div>
       ))}
-    </div>  
+    </div>
   );
 };
 
